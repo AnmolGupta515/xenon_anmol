7aac4cb9ed50f0106fa4f347a9a9088063ba294b
AnmolGupta515 <84143491+AnmolGupta515@users.noreply.github.com>
2023-12-28 20:49:53 +0530

    Add files via upload

---
+++ mobile_site/admin/contact.php
@@ -0,0 +1,23 @@
+
+
+
+
+<?php include "header.php"; ?>
+  <div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="admin-heading">Contct Us</h1>
+            </div>
+            <div class="col-md-offset-3 col-md-6">
+                <!-- FORM -->
+    
+                 <form >
+                 <label>Click here to contact us:</label>
+        <button onclick="location.href='mailto:info@example.com';">Contact Us</button>
+                   
+                </form>
+        
+        </div>
+    </div>
+</div>
---
+++ mobile_site/admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2023 Mobile Store | Powered by & Contact Us<a href="mailto:anmolshuklaganj515@gmail.com">Anmol Gupta</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
